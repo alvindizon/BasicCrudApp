95edfad9560070751740a7196a5737a0f08eca4f
Alvin Dizon <alvin.dizon91@gmail.com>
2017-12-10 17:30:57 +0800

    Added code that reads record
    Signed-off-by: Alvin Dizon <alvin.dizon91@gmail.com>

--- app/src/main/java/com/example/android/basiccrudapp/TableControllerStudent.java
+++ app/src/main/java/com/example/android/basiccrudapp/TableControllerStudent.java
@@ -2,7 +2,11 @@ package com.example.android.basiccrudapp;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Alvin Dizon on 03/12/2017.
@@ -43,6 +47,36 @@ public class TableControllerStudent extends DatabaseHandler {
         db.close();
 
         return recordCount;
+    }
+
+    public List<ObjectStudent> read(){
+        List<ObjectStudent> recordsList = new ArrayList<ObjectStudent>();
+
+        String sql = "SELECT * FROM students ORDER BY id DESC";
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(sql, null);
+
+        if(cursor.moveToFirst()){
+            do{
+                int id = Integer.parseInt(cursor.getString(cursor.getColumnIndex("id")));
+                String studentFirstname = cursor.getString(cursor.getColumnIndex("firstname"));
+                String studentEmail = cursor.getString(cursor.getColumnIndex("email"));
+
+                ObjectStudent objectStudent = new ObjectStudent();
+                objectStudent.mId = id;
+                objectStudent.mFirstName = studentFirstname;
+                objectStudent.mEmail = studentEmail;
+
+                recordsList.add(objectStudent);
+
+            } while (cursor.moveToNext());
+        }
+
+        cursor.close();
+        db.close();
+
+        return recordsList;
     }
 }
 
